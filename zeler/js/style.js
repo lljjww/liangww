38d4cc7601e4debdf6d3bce459aba71c4cd8f808
lljjww <475292755@qq.com>
2016-07-05 14:26:10 +0800

    del

---
+++ zeler/js/style.js
@@ -0,0 +1,17 @@
+/* 
+* @Author: anchen
+* @Date:   2016-04-26 10:08:43
+* @Last Modified by:   anchen
+* @Last Modified time: 2016-04-26 12:06:24
+*/
+
+$(document).ready(function(){
+    
+});
+
+var con-top = document.getElementById('con-top');
+function move(){
+
+con-top.style.backgroundColor = 'red';
+}
+move();
